c76ac72d818d912ab9697c84c394f20fff6d01b4
naoki kageyama <naoki.kp@gmail.com>
2016-12-15 16:14:27 +0900

    バージョン番号、UA修正

--- nicoalert_ver.h
+++ nicoalert_ver.h
@@ -4,11 +4,11 @@
 
 
 #define PROGRAM_NAME        _T("ニコ生アラート(Love)")
-#define VERSION_STRING      _T("v1.4.1")
-#define VERSION_STRING_RSC  "1, 4, 1, 0"
-#define VERSION_STRING_CHK  "1.4.1"
-#define VERSION_LIC         "(C) 2012-2015 naoki.kp"
-#define UA_STRING           "NicoLiveAlert(Love)/20150830/1.4.1"
+#define VERSION_STRING      _T("v1.4.2")
+#define VERSION_STRING_RSC  "1, 4, 2, 0"
+#define VERSION_STRING_CHK  "1.4.2"
+#define VERSION_LIC         "(C) 2012-2016 naoki.kp"
+#define UA_STRING           "NicoLiveAlert(Love)/20161215/1.4.2"
 //#define BETA_VERSION
 
 
